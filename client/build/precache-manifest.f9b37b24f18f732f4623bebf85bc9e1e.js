6f8e6fd1dac1a88716d5ad322610e235f65d59b5
unknown <skubidu2@inbox.ru>
2022-01-31 13:36:55 +0400

    initial commit

---
+++ client/build/precache-manifest.f9b37b24f18f732f4623bebf85bc9e1e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17ca67876a813c21c00d850bdc004852",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dc7ab2e6b9bbdfffeb03",
+    "url": "/static/css/main.b8539b04.chunk.css"
+  },
+  {
+    "revision": "f8eb1ef50d6f7b17f164",
+    "url": "/static/js/2.b2989680.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.b2989680.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc7ab2e6b9bbdfffeb03",
+    "url": "/static/js/main.91bec8c4.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "2c5552a10c3ab0a115c9484f908f982d",
+    "url": "/static/media/close.2c5552a1.svg"
+  },
+  {
+    "revision": "b3f49afdaa769bc2cb267d590e308ae0",
+    "url": "/static/media/menu.b3f49afd.svg"
+  }
+]);
---
+++ models/productModel.js
@@ -0,0 +1,69 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema(
+  {
+    product_id: {
+      type: String,
+      unique: true,
+      trim: true,
+      required: true,
+    },
+    title: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+    quantity: {
+      type: Number,
+      trim: true,
+      required: true,
+    },
+    price: {
+      type: Number,
+      trim: true,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+
+    category: {
+      type: String,
+      required: true,
+    },
+    startLocation: {
+      type: String,
+      required: true,
+    },
+    finalLocation: {
+      type: String,
+      required: true,
+    },
+    middleLocations: {
+      type: Array,
+    },
+    images: {
+      type: Object,
+      required: true,
+    },
+
+    // checked:{
+    //     type: Boolean,
+    //     default: false
+    // },
+    // sold:{
+    //     type: Number,
+    //     default: 0
+    // }
+  },
+  {
+    timestamps: true, //important
+  }
+);
+
+module.exports = mongoose.model("Products", productSchema);
