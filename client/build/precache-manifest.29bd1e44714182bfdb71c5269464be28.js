76b6bd463de126d7c70563c642eaac9c2f7d5dcc
unknown <skubidu2@inbox.ru>
2022-02-18 15:44:01 +0400

    sdfsdfsdfs

---
+++ client/build/precache-manifest.29bd1e44714182bfdb71c5269464be28.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe473db2cc18d14e74ef01d998eb76a9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "47de7e7a31803e71a9c4",
+    "url": "/static/css/main.75b71a42.chunk.css"
+  },
+  {
+    "revision": "f8eb1ef50d6f7b17f164",
+    "url": "/static/js/2.b2989680.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.b2989680.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47de7e7a31803e71a9c4",
+    "url": "/static/js/main.f81b1573.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "2c5552a10c3ab0a115c9484f908f982d",
+    "url": "/static/media/close.2c5552a1.svg"
+  },
+  {
+    "revision": "b3f49afdaa769bc2cb267d590e308ae0",
+    "url": "/static/media/menu.b3f49afd.svg"
+  }
+]);
